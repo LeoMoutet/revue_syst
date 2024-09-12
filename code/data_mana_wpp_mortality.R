fed0ab8da63c372d70dca9f3dfe6e58dd23cceda
LeoMoutet <leo.moutet@gmail.com>
2024-09-12 14:08:36 +0200

    rm population proj

--- code/data_mana_wpp_mortality.R
+++ code/data_mana_wpp_mortality.R
@@ -24,8 +24,6 @@ pacman::p_load(dplyr,
 
 # Data
 mortality_proj_wpp_2022 <- read_excel(here("data","mortality_medium_variant_wpp_2022.xlsx"))
-population_proj_wpp_2022 <- read_excel(here("data","population_medium_variant_wpp_2022.xlsx"))
-
 
 
 # Table format
@@ -47,27 +45,6 @@ mortality_proj_clean <- mortality_proj %>%
   select(c(region,Type,Year,n_death))
 
 
-## Population
-names(population_proj_wpp_2022) <- population_proj_wpp_2022[12,]
-
-population_proj <- population_proj_wpp_2022[-c(1:12),]
-
-i <- c(11:112) 
-population_proj[ , i] <- apply(population_proj[ , i], 2,  
-                               function(x) as.numeric(x))
-
-### Getting sum of pop for each region and year
-population_proj$n_pop <- rowSums( population_proj[,12:112] ) *1000      #Deaths are expressed in thousand in raw data
-
-population_proj_clean <- population_proj %>%
-  rename (region = `Region, subregion, country or area *`) %>%
-  select(c(region,Type,Year,n_pop))
-
-
-
-
-
-
 # Table for extraction grid
 
 # Polonik, 2021
@@ -76,12 +53,6 @@ mortality_proj_syst_review <- mortality_proj_clean %>%
 
 sum(mortality_proj_syst_review$n_death)
 
-
-population_proj_syst_review <- population_proj_clean %>%
-  filter(region == "WORLD" 
-         , Year == "2050")
-
-sum(population_proj_syst_review$n_pop)
 
 # Markandya
 mortality_proj_syst_review <- mortality_proj_clean %>%
@@ -104,36 +75,6 @@ mortality_proj_syst_review <- mortality_proj_clean %>%
 
 sum(mortality_proj_syst_review$n_death)
 
-
-
-population_proj_syst_review <- population_proj_clean %>%
-  filter(region == "France" 
-         , Year == "2045")
-
-sum(population_proj_syst_review$n_pop)
-
-
-# China, 2035/2050/2060 (Cai, Chen, Tang, Xing, Xie)
-population_proj_syst_review <- population_proj_clean %>%
-  filter(region == "China" 
-         , Year == "2050")
-
-sum(population_proj_syst_review$n_pop)
-
-
-population_proj_syst_review <- population_proj_clean %>%
-  filter(region == "China" 
-         , Year == "2060")
-
-sum(population_proj_syst_review$n_pop)
-sum(population_proj_syst_review$n_pop)*0.043  # Anhui
-
-population_proj_syst_review <- population_proj_clean %>%
-  filter(region == "China" 
-         , Year == "2035")
-
-sum(population_proj_syst_review$n_pop)
-
 # Chen, 2018
 mortality_proj_syst_review <- mortality_proj_clean %>%
   filter(region == "China" 
@@ -147,7 +88,6 @@ mortality_proj_syst_review <- mortality_proj_clean %>%
          , Year == "2050")
 
 sum(mortality_proj_syst_review$n_death)
-
 
 
 # Dimitrova, 2021
@@ -175,22 +115,6 @@ sum(mortality_proj_syst_review$n_death)
 
 
 # Milner, 2023 / Williams, 2018
-population_proj_syst_review <- population_proj_clean %>%
-  filter(region == "United Kingdom" 
-         , Year == "2050")
-
-sum(population_proj_syst_review$n_pop)
-
-
-population_proj_syst_review <- population_proj_clean %>%
-  filter(region == "United Kingdom" 
-         , Year == "2021" | Year == "2022" | Year == "2023" | Year == "2024" | Year == "2025" | Year == "2026" | Year == "2027" | Year == "2028" | Year == "2029" | Year == "2050" |
-           Year == "2031" | Year == "2032" | Year == "2033" | Year == "2034" | Year == "2035" | Year == "2036" | Year == "2037" | Year == "2038" | Year == "2039" | Year == "2050" |
-           Year == "2041" | Year == "2042" | Year == "2043" | Year == "2044" | Year == "2045" | Year == "2046" | Year == "2047" | Year == "2048" | Year == "2049" | Year == "2050" )
-
-sum(population_proj_syst_review$n_pop)
-
-
 mortality_proj_syst_review <- mortality_proj_clean %>%
   filter(region == "United Kingdom" , Year == "2050")
 
@@ -218,13 +142,6 @@ sum(mortality_proj_syst_review$n_death)
 
 
 # Phillips, 2021
-population_proj_syst_review <- population_proj_clean %>%
-  filter(region == "Dem. People's Republic of Korea" 
-         , Year == "2050")
-
-sum(population_proj_syst_review$n_pop)
-
-
 mortality_proj_syst_review <- mortality_proj_clean %>%
   filter(region == "Dem. People's Republic of Korea" 
          , Year == "2050")
@@ -301,21 +218,12 @@ mortality_proj_syst_review <- mortality_proj_clean %>%
 
 sum(mortality_proj_syst_review$n_death)
 
-
-
 # Shindell, 2020
 mortality_proj_syst_review <- mortality_proj_clean %>%
   filter(region == "United States of America" 
          , Year == "2050")
 
 sum(mortality_proj_syst_review$n_death)
-
-population_proj_syst_review <- population_proj_clean %>%
-  filter(region == "United States of America" 
-         , Year == "2050")
-
-sum(population_proj_syst_review$n_pop)
-
 
 # Shindell, 2018
 mortality_proj_syst_review <- mortality_proj_clean %>%
@@ -341,13 +249,6 @@ sum(mortality_proj_syst_review$n_death)
 
 
 # Zyzk, 2020
-population_proj_syst_review <- population_proj_clean %>%
-  filter(region == "Poland" 
-         , Year == "2050")
-
-sum(population_proj_syst_review$n_pop)
-
-
 mortality_proj_syst_review <- mortality_proj_clean %>%
   filter(region == "Poland" 
          , Year == "2050")
